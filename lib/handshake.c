ee21442460758bc98ee30b1938b7112530e8e7c6
sroshc <shirzaysroshsafa@gmail.com>
2024-11-14 13:25:31 -0500

    Started on encryption library

--- lib/handshake.c
+++ lib/handshake.c
@@ -3,10 +3,30 @@
 #include <openssl/err.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
+#include <openssl/rand.h>
 #include <string.h>
 #include <stdio.h>
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+typedef struct Sym_key{
+    unsigned char key[256];
+    unsigned char iv[128];
+
+}Sym_key;
+
+Sym_key* init_key(){
+    Sym_key *k = malloc(sizeof(Sym_key));
+    RAND_bytes(k->key, 256);
+    RAND_bytes(k->iv, 128);
+
+    return k;
+} 
+
+void handleErrors(){
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+int _sym_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
             unsigned char *iv, unsigned char *ciphertext)
 {
     EVP_CIPHER_CTX *ctx;
@@ -51,7 +71,7 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
     return ciphertext_len;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+int _sym_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
             unsigned char *iv, unsigned char *plaintext)
 {
     EVP_CIPHER_CTX *ctx;
@@ -94,5 +114,92 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
     EVP_CIPHER_CTX_free(ctx);
 
     return plaintext_len;
+}
+
+unsigned char* sym_encrypt(Sym_key *k, char* plaintext, int* ciphertext_len){
+    int plaintext_len = strlen(plaintext);
+
+    unsigned char *ciphertext = malloc(plaintext_len + EVP_MAX_BLOCK_LENGTH);
+
+    *ciphertext_len = _sym_encrypt((unsigned char *)plaintext, strlen(plaintext), k->key, k->iv, ciphertext);
+    
+    ciphertext = realloc(ciphertext, *ciphertext_len);
+
+
+    return ciphertext;
+}
+
+char *sym_decrypt(Sym_key *k, unsigned char* ciphertext, int ciphertext_len){
+    int decryptedtext_len;
+
+    char *decryptedtext = malloc(ciphertext_len + 1);
+
+    decryptedtext_len = _sym_decrypt(ciphertext, ciphertext_len, k->key, k->iv, (unsigned char *)decryptedtext);
+   
+    decryptedtext[decryptedtext_len] = '\0';
+
+    return decryptedtext;
+}
+
+
+
+
+int main (void)
+{
+    Sym_key* key = init_key();
+    int ciphertext_len;
+
+    unsigned char* ciphertext = sym_encrypt(key, "The quick brown fox jumps over the lazy dog.", &ciphertext_len);
+    char* decryptedtext = sym_decrypt(key, ciphertext, ciphertext_len);
+    printf("%s\n", decryptedtext);
+
+    // /*
+    //  * Set up the key and iv. Do I need to say to not hard code these in a
+    //  * real application? :-)
+    //  */
+
+    // /* A 256 bit key */
+    // unsigned char key[256];
+
+    // /* A 128 bit IV */
+    // unsigned char iv[128];
+
+    // RAND_bytes(key, 256);
+    // RAND_bytes(iv, 128);
+
+    // /* Message to be encrypted */
+    // char *plaintext =
+    //         "The quick brown fox jumps over the lazy dog";
+    // /*
+    //  * Buffer for ciphertext. Ensure the buffer is long enough for the
+    //  * ciphertext which may be longer than the plaintext, depending on the
+    //  * algorithm and mode.
+    //  */
+    // unsigned char ciphertext[128];
+
+    // /* Buffer for the decrypted text */
+    // unsigned char decryptedtext[128];
+
+    // int decryptedtext_len, ciphertext_len;
+
+    // /* Encrypt the plaintext */
+    // ciphertext_len = _sym_encrypt ((unsigned char*)plaintext, strlen (plaintext), key, iv,
+    //                           ciphertext);
+
+    // /* Do something useful with the ciphertext here */
+
+    // /* Decrypt the ciphertext */
+    // decryptedtext_len = _sym_decrypt(ciphertext, ciphertext_len, key, iv,
+    //                             decryptedtext);
+
+    // /* Add a NULL terminator. We are expecting printable text */
+    // decryptedtext[decryptedtext_len] = '\0';
+
+    // /* Show the decrypted text */
+    // printf("Decrypted text is:\n");
+    // printf("%s\n", decryptedtext);
+    // printf("Actual text: %s\n", plaintext);
+
+    // return 0;
 }
 
