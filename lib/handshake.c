b542d06bf7cdb03e40f57c094f8f12b0e38cd0bc
sroshc <shirzaysroshsafa@gmail.com>
2024-11-15 01:32:28 -0500

    RSA encryption added

--- lib/handshake.c
+++ lib/handshake.c
@@ -7,26 +7,36 @@
 #include <string.h>
 #include <stdio.h>
 
+#define ENCRYPTED_LENGTH 256
+
 typedef struct Sym_key{
-    unsigned char key[256];
-    unsigned char iv[128];
-
+    unsigned char key[32];
+    unsigned char iv[16];
 }Sym_key;
-
-Sym_key* init_key(){
+/*
+typedef struct RSA_key{
+    unsigned char
+}
+*/
+Sym_key* init_sym_key(){
     Sym_key *k = malloc(sizeof(Sym_key));
-    RAND_bytes(k->key, 256);
-    RAND_bytes(k->iv, 128);
+    RAND_bytes(k->key, 32);
+    RAND_bytes(k->iv, 16);
 
     return k;
 } 
+
+void free_sym_key(Sym_key* key){
+    free(key);
+    return;
+}
 
 void handleErrors(){
     ERR_print_errors_fp(stderr);
     abort();
 }
 
-int _sym_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+static int _sym_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
             unsigned char *iv, unsigned char *ciphertext)
 {
     EVP_CIPHER_CTX *ctx;
@@ -71,7 +81,7 @@ int _sym_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key
     return ciphertext_len;
 }
 
-int _sym_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+static int _sym_decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
             unsigned char *iv, unsigned char *plaintext)
 {
     EVP_CIPHER_CTX *ctx;
@@ -141,17 +151,150 @@ char *sym_decrypt(Sym_key *k, unsigned char* ciphertext, int ciphertext_len){
     return decryptedtext;
 }
 
+typedef struct RSA_key{
+    EVP_PKEY* privatekey;
+    EVP_PKEY* publickey;
+    EVP_PKEY_CTX* ctx;
+}RSA_key;
+
+RSA_key* init_rsa_key(char* private_file, char* public_file){
+    RSA_key* key = malloc(sizeof(RSA_key));
+    FILE *pFile;
 
 
+    pFile = fopen(private_file, "r");
+    if (!pFile) {
+        perror("Error opening private key file");
+        free(key);
+        return NULL;
+    }
+    key->privatekey = PEM_read_PrivateKey(pFile, NULL, NULL, NULL);
+    fclose(pFile);
+    if (!key->privatekey) {
+        handleErrors();
+    }
+    
+    // Load the public key
+    pFile = fopen(public_file, "r");
+    if (!pFile) {
+        perror("Error opening public key file");
+        free(key->privatekey);
+        free(key);
+        return NULL;
+    }
 
-int main (void)
-{
-    Sym_key* key = init_key();
+    key->publickey = PEM_read_PUBKEY(pFile, NULL, NULL, NULL);
+    if (!key->privatekey) {
+        handleErrors();
+    }
+
+    fclose(pFile);
+
+
+    key->ctx = EVP_PKEY_CTX_new(key->publickey, NULL);
+    if (!key->ctx) {
+        handleErrors();
+    }
+    
+    if (EVP_PKEY_encrypt_init(key->ctx) <= 0) {
+        handleErrors();
+    }
+
+    return key;
+}
+
+void change_ctx(RSA_key* key, EVP_PKEY* pkey){
+    EVP_PKEY_CTX_free(key->ctx);
+    key->ctx = EVP_PKEY_CTX_new(pkey, NULL);
+    if (!key->ctx) {
+        handleErrors();
+    }
+    
+    return;
+}
+
+void free_rsa_key(RSA_key* key){
+    EVP_PKEY_free(key->privatekey);
+    EVP_PKEY_free(key->publickey);
+    EVP_PKEY_CTX_free(key->ctx);
+    free(key);
+
+    return;
+}
+
+void test_sym_encrypt(){
+    Sym_key* key = init_sym_key();
     int ciphertext_len;
 
     unsigned char* ciphertext = sym_encrypt(key, "The quick brown fox jumps over the lazy dog.", &ciphertext_len);
     char* decryptedtext = sym_decrypt(key, ciphertext, ciphertext_len);
     printf("%s\n", decryptedtext);
+}
+
+
+int main(void) {
+    RSA_key* key = init_rsa_key("private_key.pem", "public_key.pem");
+    
+    unsigned char *encrypted = NULL, *decrypted = NULL;
+    unsigned char data[] = "Hello, this is a test message!";
+    size_t encrypted_length, decrypted_length;
+    
+    
+    if (EVP_PKEY_encrypt(key->ctx, NULL, &encrypted_length, data, strlen((char *)data)) <= 0) {
+        handleErrors();
+    }
+    
+    encrypted = malloc(encrypted_length);
+
+    if (!encrypted) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+    
+    // Perform the actual encryption
+    if (EVP_PKEY_encrypt(key->ctx, encrypted, &encrypted_length, data, strlen((char *)data)) <= 0) {
+        handleErrors();
+    }
+    
+    printf("Encrypted data length: %zu\n", encrypted_length);
+    
+    
+    change_ctx(key, key->privatekey);
+    
+
+    if (EVP_PKEY_decrypt_init(key->ctx) <= 0) {
+        handleErrors();
+    }
+    
+    // Determine the buffer size required for decrypted data
+    if (EVP_PKEY_decrypt(key->ctx, NULL, &decrypted_length, encrypted, ENCRYPTED_LENGTH) <= 0) {
+        handleErrors();
+    }
+    
+    decrypted = malloc(decrypted_length+1);
+    if (!decrypted) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+    
+    // Perform the actual decryption
+    if (EVP_PKEY_decrypt(key->ctx, decrypted, &decrypted_length, encrypted, encrypted_length) <= 0) {
+        handleErrors();
+    }
+    decrypted[decrypted_length] = '\0';
+    
+    printf("Decrypted message: %s\n", decrypted);
+    
+    // Clean up
+    free_rsa_key(key);
+    free(encrypted);
+    free(decrypted);
+    
+    return 0;
+}
+
+
+
 
     // /*
     //  * Set up the key and iv. Do I need to say to not hard code these in a
@@ -201,5 +344,3 @@ int main (void)
     // printf("Actual text: %s\n", plaintext);
 
     // return 0;
-}
-
